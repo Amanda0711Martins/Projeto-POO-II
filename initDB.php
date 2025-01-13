a063148bc3f2c0fc21e077d681a6cfc44176c3f9
Amanda Martins <amanda_nelva07@hotmail.com>
2025-01-13 12:38:38 -0300

    muitas alterações

---
+++ initDB.php
@@ -0,0 +1,20 @@
+<?php/*
+namespace database;
+use PDO;
+use PDOException;
+
+$endereco = 'localhost';
+$porta = '5432';
+$banco = 'postgres';
+$usuario = 'postgres';
+$senha = 'Minas@0202';
+
+try {
+  $pdo = new PDO("pgsql:host=$endereco;port=$porta;dbname=$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+} catch (PDOException $e) {
+  echo "Falha ao conectar ao banco de dados. <br/>";
+  die($e->getMessage());
+}
+*/
+?>
