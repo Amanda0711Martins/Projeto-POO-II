d07b8168bf71f1d43a56edf8eca95a41590ed507
Amanda Martins <amanda_nelva07@hotmail.com>
2025-01-13 21:08:31 -0300

    alteração no initDB

--- initDB.php
+++ initDB.php
@@ -9,20 +9,6 @@ $banco = 'postgres';
 $usuario = 'postgres';
 $senha = 'Minas@0202';
 
-
-if (extension_loaded('pdo_pgsql')) {
-    echo "A extensão pdo_pgsql está habilitada.";
-} else {
-    echo "A extensão pdo_pgsql NÃO está habilitada.";
-}
-
-if (extension_loaded('pgsql')) {
-    echo "A extensão pgsql está habilitada.";
-} else {
-    echo "A extensão pgsql NÃO está habilitada.";
-}
-
-
 try {
   $pdo = new PDO("pgsql:host=$endereco;port=$porta;dbname=$banco", $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
