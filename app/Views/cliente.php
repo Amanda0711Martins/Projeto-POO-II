f71bef30b16da76b994c41b4344e5966e964364b
Amanda Martins <amanda_nelva07@hotmail.com>
2025-02-16 21:49:14 -0300

    inserção do arquivo cliente.css

--- app/Views/cliente.php
+++ app/Views/cliente.php
@@ -19,6 +19,7 @@ if (!$cliente) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalhes do Cliente</title>
+    <link rel="stylesheet" href="/css/clientes.css" >
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
